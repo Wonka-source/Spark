7a1d3c4fbb31b2c974cc697dfd0058b7e6a70a4f
Wonka-source <dan.eff117@gmail.com>
2023-01-31 00:37:47 +0000

    add view and url's for user profile

--- app_spark/urls.py
+++ app_spark/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.EventList.as_view(), name='home'),
     path('about/', views.about, name='about'),
-    # path('profile/', views.view_profile, name='profile'),
     path('create_event', views.create_event, name='create_event'),
+    path('user_profile/', views.UserProfile.as_view(), name='user_profile'),
+
 ]
--- app_spark/views.py
+++ app_spark/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
 from .models import Event
 from django.contrib.auth.decorators import login_required
 from .forms import EventForm
+from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class EventList(generic.ListView):
@@ -12,21 +14,28 @@ class EventList(generic.ListView):
     paginate_by = 6
 
 
+class UserProfile(LoginRequiredMixin, View):
+    
+    def get(self, request):
+        user_event_list = Event.objects.filter(promoter=request.user).all().order_by('-created_on')
+        today = timezone.now()
+        return render(
+            request,
+            "user_profile.html",
+            {
+                "user_event_list": user_event_list,
+                'page_title': 'Profile',
+                'today': today,
+            }
+        )
+
+
 def about(request):
 
     return render(
         request,
         "about.html",
 )
-
-
-# @login_required(login_url='/accounts/login/')
-# def view_profile(request):
-#     context = {
-#         'user': request.user
-
-#     }
-#     return render(request, 'profile.html', context)
 
 
 @login_required(login_url='/accounts/login/')
